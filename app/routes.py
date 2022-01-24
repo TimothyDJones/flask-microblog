cb528c236421f586245d359906dfba8b891130ad
Tim Jones <tdjones74021@yahoo.com>
2022-01-24 16:12:51 -0600

    Additional clean up of login process.

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user
+from werkzeug.urls import url_parse
 
 from app import app
 from app.forms import LoginForm
@@ -7,6 +8,7 @@ from app.models import User
 
 @app.route("/")
 @app.route("/index")
+@login_required
 def index():
 #    return "Hello, world!"
     user = {"username": "Tim"}
@@ -20,7 +22,7 @@ def index():
             "body": "The Avengers movie was so cool!"
         }
     ]
-    return render_template("index.html", title="Home", user=user, posts=posts)
+    return render_template("index.html", title="Home", posts=posts)
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -35,7 +37,10 @@ def login():
             flash("Invalid user name or password!")
             return redirect(url_for("index"))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for("index"))
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netlog != "":
+            next_page = url_for("index")
+        return redirect(next_page)
     return render_template("login.html", title="Sign In", form=form)
 
 @app.route("/logout")
