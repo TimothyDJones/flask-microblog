97e0743e7e30ecb7b118a8ecda6db91b7df6c5f5
Tim Jones <tdjones74021@yahoo.com>
2022-01-24 15:16:08 -0600

    Add basic Flask-Login support.

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,9 @@
-from time import sleep
+from flask import render_template, flash, redirect, url_for
+from flask_login import current_user, login_user, logout_user
 
-from flask import render_template, flash, redirect
 from app import app
 from app.forms import LoginForm
+from app.models import User
 
 @app.route("/")
 @app.route("/index")
@@ -23,9 +24,21 @@ def index():
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
     form = LoginForm()
     if form.validate_on_submit():
-        flash("Log in requested for user {user}, remember_me={r}"
-            .format(user=form.username.data, r=form.remember_me.data))
+#        flash("Log in requested for user {user}, remember_me={r}"
+#            .format(user=form.username.data, r=form.remember_me.data))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash("Invalid user name or password!")
+            return redirect(url_for("index"))
+        login_user(user, remember=form.remember_me.data)
         return redirect(url_for("index"))
     return render_template("login.html", title="Sign In", form=form)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("index"))
